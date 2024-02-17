d2eb410ba9da70a4138f21d4f1840114f82ca512
debasis724 <sakhidev724@gmail.com>
2024-02-17 18:39:16 +0530

    Weather App v1.0

---
+++ weatherApp.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__, template_folder='/home/debasis/mysite/templates')
+
+api_key = 'a5457a4587a8a0f123d4b9fa587b2a50'
+weather_api_url = 'http://api.openweathermap.org/data/2.5/weather'
+
+@app.route('/')
+def index():
+    return render_template('look.html')
+
+@app.route('/weather/<city>', methods=['GET'])
+def get_weather(city):
+    '''city = request.form['city']'''
+    params = {'q': city, 'appid': api_key, 'units': 'metric'}
+
+    response = requests.get(weather_api_url, params=params)
+    weather_data = json.loads(response.text)
+
+    if response.status_code == 200:
+        temperature = weather_data['main']['temp']
+        description = weather_data['weather'][0]['description']
+        return render_template('result.html', city=city, temperature=temperature, description=description)
+    else:
+        return render_template('error.html', error_message=f"Unable to fetch weather data for {city}")
+
+if __name__ == '__main__':
+    app.run(port = 5002)
+
+
+
+
+
+
